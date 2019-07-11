c637b1c971363b38f57d41b5aec97ce90200c5c0
iharir <iharir@largus.ma>
2019-07-11 17:22:48 +0100

    bugfix

--- public/index.php
+++ public/index.php
@@ -7,8 +7,9 @@ require '../vendor/autoload.php';
 require '../src/IoC/initIoC.php';
 require '../config/routes/aggregator.php';
 
-$url = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : "";
+$url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : "";
 $method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : null;
+$url = explode("?", $url)[0];
 
 if (is_string($method) && !empty($method)) {
     try {
